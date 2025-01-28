dd6480ba94985010d550179a3a277a63c5a1fccb
Hemma Lekha <[hemmalekha.r2023cse@sece.ac.in]>
2025-01-28 10:29:36 +0530

    Adding backend files

--- seceBackend/index.js
+++ seceBackend/index.js
@@ -1,110 +1,74 @@
-const express = require('express');
-const path = require('path');
-const mdb = require('mongoose');
-const dotenv = require('dotenv');
+const express = require("express");
+const path = require("path");
+const mdb = require("mongoose");
+const dotenv = require("dotenv");
 const Signup = require("./models/signupSchema");
-const app = express();
+const signup_schema = require("./models/signupSchema");
 dotenv.config();
-
+const app = express();
 app.use(express.json());
 
-mdb.connect(process.env.MONGODB_URL)
+mdb
+  .connect("mongodb+srv://Mern2025:mern2025@mern2025.erlau.mongodb.net/")
   .then(() => {
-    console.log("MongoDB Connection Successful");
+    console.log("MongoDB Connection Sucessfull");
   })
   .catch((err) => {
-    console.log("MongoDb connection unsuccessful", err);
+    console.log("MongoDB Connection Unsucessfull", err);
   });
 
-app.get('/', (req, res) => {
-  res.send("Welcome to Backend friends");
+app.get("/", (req, res) => {
+  res.send(
+    "Welcome to Backend my friend\n Your RollerCoster starts from now on\n Fasten your codabase so you can catchup of what is been taught"
+  );
 });
-
-app.get('/static', (req, res) => {
+app.get("/static", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// Signup Route
-app.post('/signup', (req, res) => {
-  var { firstname, lastname, username, email, password } = req.body;
+app.post("/signup", (req, res) => {
+  var { firstName, lastName, username, email, password } = req.body;
   try {
-    console.log("inside try");
-    const newCustomer = new Signup({
-      firstname: firstname,
-      lastname: lastname,
+console.log("Inside try");
+      const newCustomer = new Signup({
+      firstName: firstName,
+      lastName: lastName,
       username: username,
       email: email,
       password: password,
     });
-
-    console.log(newCustomer);
-    newCustomer.save();
-    res.status(201).send("Signup successful");
+    newCustomer.save()
+    res.status(201).send("Signup Successfull");
   } catch (err) {
-    res.status(400).send("Signup unsuccessful", err);
+    res.status(400).send("Signup Unsuccessfull",err);
   }
 });
 
-// Login Route
-app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
+app.get('/getsignupdet',async(req,res)=>{
+  var signUpdet=await(Signup.find())
+  res.status(200).json(signUpdet);
+})
 
-  try {
-    const user = await Signup.findOne({ email: email });
-    if (!user) {
-      return res.status(404).send("User not found");
-    }
+app.post('/updatedet',async(req,res)=>{
+  var updateRec= await Signup.findOneAndUpdate(
+    {username:"Deekshu"},
+    {$set:{username:"Deekshu_12"}}
+  )
+  console.log(updateRec)
+  updateRec.save()
+  res.json("Record updated")
+})
 
-    if (user.password === password) {
-      res.status(200).send("Login successful");
+app.post('/deletedet', async (req, res) => {
+  const { username } = req.body; 
+  const deleteRec = await Signup.findOneAndDelete({ username: "Vijay-22" }); 
+    if (deleteRec) {
+      res.json("Record deleted successfully" );
     } else {
-      res.status(401).send("Incorrect password");
+      res.json("Record not found");
     }
-  } catch (err) {
-    res.status(500).send("Error during login");
-  }
 });
 
-// Get Signup Details Route
-app.get('/getsignupdet', async (req, res) => {
-  try {
-    const signUpdet = await Signup.find();
-    res.status(200).json(signUpdet);
-  } catch (err) {
-    res.status(500).send("Error fetching signup details");
-  }
-});
-// Update User Details Route
-app.put('/updateuser', async (req, res) => {
-    const { id, ...updates } = req.body; // Extract id and update fields
-  
-    try {
-      const updatedUser = await Signup.findByIdAndUpdate(id, updates, { new: true });
-      if (!updatedUser) {
-        return res.status(404).send("User not found");
-      }
-      res.status(200).send("User details updated successfully");
-    } catch (err) {
-      res.status(500).send("Error updating user details");
-    }
-  });
-  
-  
-  // Delete User Route
-  app.delete('/deleteuser', async (req, res) => {
-    const { id } = req.body; // Extract id from request body
-  
-    try {
-      const deletedUser = await Signup.findByIdAndDelete(id);
-      if (!deletedUser) {
-        return res.status(404).send("User not found");
-      }
-      res.status(200).send("User deleted successfully");
-    } catch (err) {
-      res.status(500).send("Error deleting user");
-    }
-  });
-  
 app.listen(3001, () => {
-  console.log("Server connected on port 3001");
+  console.log("Server Started");
 });
